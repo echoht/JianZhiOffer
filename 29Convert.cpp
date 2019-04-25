3bea0759b79390d05c8ab037fc8e3bdd45577213
echoht <48375360+echoht@users.noreply.github.com>
2019-04-25 12:14:21 +0800

    Create 29Convert.cpp

---
+++ 29Convert.cpp
@@ -0,0 +1,71 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+   //find the max node in left sub tree , left point into the node   
+	//root is already the left sub tree
+	TreeNode* leftMaxNode(TreeNode * root){
+		if(root == NULL){
+			return NULL;
+		}
+		if(root->right == NULL){
+			return root;
+		}
+		return leftMaxNode(root->right);
+	}
+	// find the min node in right sub tree, right point into the node
+	//root is already the right sub tree
+	TreeNode* rightMinNode(TreeNode * root){
+		if(root == NULL){
+			return NULL;
+		}
+		if(root->left == NULL){
+			return root;
+		}
+		return rightMinNode(root->left);
+	}
+	
+	TreeNode* conv(TreeNode* root){
+		if(root == NULL){
+			return NULL;
+		}
+		TreeNode* left = root->left;
+		TreeNode* right = root->right;
+		TreeNode* lmn = leftMaxNode(left);
+		TreeNode* rmn = rightMinNode(right);
+		
+        if(root->left == NULL || root->left->right == root){
+            //nothing 
+            ;
+        }else{
+            root->left = lmn;
+			lmn->right = root;
+			conv(left);
+            
+        }
+        if(root->right == NULL || root->right->left == root){
+            //nothing
+            ;
+        }else{
+            root->right = rmn;
+			rmn->left = root;
+			conv(right);
+        }
+		return root;
+	}
+    TreeNode* Convert(TreeNode* pRootOfTree)
+    {
+        TreeNode* tmp = conv(pRootOfTree);
+        while(tmp!= NULL &&　tmp->left != NULL){
+			tmp = tmp->left;
+		}
+		return tmp;
+    }
+};
